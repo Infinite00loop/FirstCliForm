88db3bfc00b26b8a3453d501fd575012e3e57f47
Infinite00loop <samriddhisingh07092002@gmail.com>
2023-10-02 18:02:05 +0530

    appointment app

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
-function printDetails(){
+function onSignUp(){
+    var name_=document.getElementById('idx1').value;
+    var email_=document.getElementById('idx2').value;
+    var phone_=document.getElementById('idx3').value;
+
+    let myObj={
+        name:name_,
+        email:email_,
+        phone:phone_
+    };
+
+    var myObjSerial=JSON.stringify(myObj);
+    localStorage.setItem(email_,myObjSerial);
+
+    var list=document.getElementById('list-items');
+    var newList=document.createElement('li');
+    var text=name_+" - "+email_+" - "+phone_;
+    newList.appendChild(document.createTextNode(text));
+    list.appendChild(newList);
+
     // localStorage.setItem('Name',document.getElementById('idx1').value)
     // localStorage.setItem('Email',document.getElementById('idx2').value)
     // localStorage.setItem('Phone',document.getElementById('idx3').value)
@@ -12,13 +31,13 @@ function printDetails(){
 
 }
 
-let myObj= {
-    name : 'Sam',
-    age: '20'
-};
+// let myObj= {
+//     name : 'Sam',
+//     age: '20'
+// };
 
-let myObjSerialized=JSON.stringify(myObj);
-localStorage.setItem('myObj',myObjSerialized);
-console.log(localStorage.getItem('myObj'))
-let myObjDeserialized=JSON.parse(localStorage.getItem('myObj'));
-console.log(myObjDeserialized)
+// let myObjSerialized=JSON.stringify(myObj);
+// localStorage.setItem('myObj',myObjSerialized);
+// console.log(localStorage.getItem('myObj'))
+// let myObjDeserialized=JSON.parse(localStorage.getItem('myObj'));
+// console.log(myObjDeserialized)
