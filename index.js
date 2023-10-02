8310564671014fe1ae536440aabb433c02469ccd
Infinite00loop <samriddhisingh07092002@gmail.com>
2023-10-02 14:05:24 +0530

    local Storage

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 function printDetails(){
-    console.log(document.getElementById('idx1').value);
-    console.log(document.getElementById('idx2').value);
-    console.log(document.getElementById('idx3').value);
-    console.log(document.getElementById('idx4').value);
-    console.log(document.getElementById('idx5').value);
+    localStorage.setItem('Name',document.getElementById('idx1').value)
+    localStorage.setItem('Email',document.getElementById('idx2').value)
+    localStorage.setItem('Phone',document.getElementById('idx3').value)
+    localStorage.setItem('Date',document.getElementById('idx4').value)
+    localStorage.setItem('Time',document.getElementById('idx5').value)
+    console.log(localStorage.getItem('Name'));
+    console.log(localStorage.getItem('Email'));
+    console.log(localStorage.getItem('Phone'));
+    console.log(localStorage.getItem('Date'));
+    console.log(localStorage.getItem('Time'));
 }
