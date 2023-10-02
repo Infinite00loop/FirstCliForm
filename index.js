c11b3536b0afeb88b6f761d3a6f050cba27f19f1
Infinite00loop <samriddhisingh07092002@gmail.com>
2023-10-02 16:57:27 +0530

    object local storage

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 function printDetails(){
-    localStorage.setItem('Name',document.getElementById('idx1').value)
-    localStorage.setItem('Email',document.getElementById('idx2').value)
-    localStorage.setItem('Phone',document.getElementById('idx3').value)
-    localStorage.setItem('Date',document.getElementById('idx4').value)
-    localStorage.setItem('Time',document.getElementById('idx5').value)
-    console.log(localStorage.getItem('Name'));
-    console.log(localStorage.getItem('Email'));
-    console.log(localStorage.getItem('Phone'));
-    console.log(localStorage.getItem('Date'));
-    console.log(localStorage.getItem('Time'));
+    // localStorage.setItem('Name',document.getElementById('idx1').value)
+    // localStorage.setItem('Email',document.getElementById('idx2').value)
+    // localStorage.setItem('Phone',document.getElementById('idx3').value)
+    // localStorage.setItem('Date',document.getElementById('idx4').value)
+    // localStorage.setItem('Time',document.getElementById('idx5').value)
+    // console.log(localStorage.getItem('Name'));
+    // console.log(localStorage.getItem('Email'));
+    // console.log(localStorage.getItem('Phone'));
+    // console.log(localStorage.getItem('Date'));
+    // console.log(localStorage.getItem('Time'));
+
 }
+
+let myObj= {
+    name : 'Sam',
+    age: '20'
+};
+
+let myObjSerialized=JSON.stringify(myObj);
+localStorage.setItem('myObj',myObjSerialized);
+console.log(localStorage.getItem('myObj'))
+let myObjDeserialized=JSON.parse(localStorage.getItem('myObj'));
+console.log(myObjDeserialized)
